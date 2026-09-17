a8771d6c798fb31688a7956c2e76ce612854af77
Alexaa-dt <alexandradazatenorio06@gmail.com>
2026-09-17 18:08:44 +0200

    Intento de empezar la clase Store

--- src/tiendaJuegos/Store.java
+++ src/tiendaJuegos/Store.java
@@ -1,5 +1,19 @@
 package tiendaJuegos;
 
+import java.util.ArrayList;
+
 public class Store {
 
+	private ArrayList<Game> games;
+	private ArrayList<Customer> customers;
+	private ArrayList<Purchase> purchases;
+
+	public Store() {
+		games = new ArrayList<Game>();
+		customers = new ArrayList<Customer>();
+		purchases = new ArrayList<Purchase>();
+	}
+	
+	
+	
 }
