5cdccfe9d8fe3f408d7cc5be4cede739831f5174
Alexaa-dt <alexandradazatenorio06@gmail.com>
2026-09-17 17:51:47 +0200

    Realice clase Customer y Purchase

--- src/tiendaJuegos/Customer.java
+++ src/tiendaJuegos/Customer.java
@@ -2,25 +2,68 @@ package tiendaJuegos;
 
 public class Customer {
 	
+	// no he usado excepciones aqui, acordar implementarlas
+
+	private static int contador = 0;
+
 	private int id;
-	private String name;
-	private double balance;
-	
-	
-	
+	private String nombre;
+	private double saldo;
+
+	public Customer(String nombre, double saldo) {
+		contador++;
+		this.id = contador;
+		this.nombre = nombre;
+
+		if (saldo >= 0) {
+			this.saldo = saldo;
+		} else {
+			this.saldo = 0;
+		}
+	}
+
 	public int getId() {
 		return id;
 	}
-	public String getName() {
-		return name;
-	}
-	
-	public double getBalance() {
-		return balance;
-	}
-	
-	public void anadirSaldo(double balance) {
-		
 
-}
+	public String getNombre() {
+		return nombre;
+	}
+
+	public double consultarSaldo() {
+		return saldo;
+	}
+
+	public void añadirSaldo(double cantidad) {
+		if (cantidad > 0) {
+			saldo = saldo + cantidad;
+		} else {
+			System.out.println("La cantidad no es válida");
+		}
+	}
+
+	public void retirarSaldo(double cantidad) {
+		if (cantidad > 0) {
+			if (cantidad <= saldo) {
+				saldo = saldo - cantidad;
+			} else {
+				System.out.println("No hay suficiente saldo");
+			}
+		} else {
+			System.out.println("La cantidad no es válida");
+		}
+	}
+
+	public boolean tieneSaldo(double cantidad) {
+		if (saldo >= cantidad) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente: " + nombre + ", ID: " + id + ", Saldo: " + saldo;
+	}
 }
--- src/tiendaJuegos/Purchase.java
+++ src/tiendaJuegos/Purchase.java
@@ -2,4 +2,44 @@ package tiendaJuegos;
 
 public class Purchase {
 
+	private Customer cliente;
+	private Game juego;
+	private int quantity;
+	private double totalprice;
+
+	public Purchase(Customer cliente, Game juego, int quantity) {
+
+		this.cliente = cliente;
+		this.juego = juego;
+		this.quantity = quantity;
+		
+		/*/Aquí calculo el precio total de la compra Cojo el precio 
+		 * del juego y lo multiplico por la cantidad que se ha comprado, y lo guardo en precioTotal
+		 */
+
+		this.totalprice = juego.getPrice() * quantity;
+	}
+
+	public Customer getCliente() {
+		return cliente;
+	}
+
+	public Game getJuego() {
+		return juego;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public double getTotalprice() {
+		return totalprice;
+	}
+
+	@Override
+	public String toString() {
+		return "Purchase{" + "cliente=" + cliente + ", juego=" + juego + ", cantidad=" + quantity + ", precioTotal="
+				+ totalprice + '}';
+	}
+
 }
